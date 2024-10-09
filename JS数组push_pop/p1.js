fe358998863a4ea0116fe71b944f413c98cd24e3
Huang_Feiyang <1552472225@qq.com>
2024-10-09 09:19:35 +0800

    10.9_上午9:19_更改

--- JS数组push_pop/p1.js
+++ JS数组push_pop/p1.js
@@ -2,7 +2,7 @@ var arr = [1, 2, 3];
 arr.push(4);
 document.write(arr); // 1,2,3,4
 console.log(arr); // [1, 2, 3, 4]
-arr.pop();
+arr.pop();//删除最后一个元素
 document.write("<br>"); // 1,2,3
 document.write(arr); // 1,2,3
 console.log(arr); // [1, 2, 3]
